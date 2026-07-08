7654a8e68637d9391c474ae163733387f002777f
LoaiRadwan <loairadwan6@gmail.com>
2026-07-08 11:31:06 +0100

    add static root

--- promptHub/settings.py
+++ promptHub/settings.py
@@ -120,5 +120,6 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
