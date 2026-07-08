bf262a8984fad924ef776c11ac700454eff09bbb
LoaiRadwan <loairadwan6@gmail.com>
2026-07-08 12:10:03 +0100

    .

--- promptHub/settings.py
+++ promptHub/settings.py
@@ -119,7 +119,7 @@ MESSAGE_TAGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
